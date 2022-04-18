f58c93f54e3c0131885aa6497a81304f1f5ceae1
Ankie Pon <anpon@ucsc.edu>
2022-04-18 01:50:47 -0700

    made changes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
+//configuration
 let config = {
     type: Phaser.CANVAS,
-    width: 1648,
-    height: 975,
+    width: 640,
+    height: 480,
     scene: [ Menu, Play ]
 }
 
+//game declaration
 let game = new Phaser.Game(config);
 
-// set UI sizes
-let borderUISize = game.config.height / 15;
+//reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+//set UI sizes
+let borderUISize= game.config.height / 15;
 let borderPadding = borderUISize / 3;
